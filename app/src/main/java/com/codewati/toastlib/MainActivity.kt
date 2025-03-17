552780ac6f69150a41bc5a7d2ee8c7ecb7e03d31
Abhivishrut Bishnoi <80168406+abhivishrut@users.noreply.github.com>
2025-03-17 19:20:14 +0530

    update

---
+++ app/src/main/java/com/codewati/toastlib/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.codewati.toastlib
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.codewati.customtoast.Toaster
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toaster.shortToast(this, "Hello world!")
+
+    }
+}
