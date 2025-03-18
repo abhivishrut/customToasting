07f20b9486fcafb94dcbbcc6b6e97989aaadb839
Abhivishrut Bishnoi <80168406+abhivishrut@users.noreply.github.com>
2025-03-18 14:53:12 +0530

    update 1.0.3

--- app/src/main/java/com/codewati/toastlib/MainActivity.kt
+++ app/src/main/java/com/codewati/toastlib/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.codewati.toastlib
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.codewati.customtoast.Toaster
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +12,20 @@ class MainActivity : AppCompatActivity() {
 
         Toaster.shortToast(this, "Hello world!")
         Toaster.longToast(this, "This is Codewati :)")
+        Toaster.doneToast(this, "Codewati launches Successfully")
+        Toaster.alertToast(this, "Codewati launches Successfully")
+        Toaster.warningToast(this, "Codewati launches Successfully")
+        Toaster.loadingToast(this, "Codewati launches Successfully")
+        Toaster.infoToast(this, "Codewati launches Successfully")
+
+        val rootView: View = window.decorView.findViewById(android.R.id.content)
+
+        Toaster.doneSnackbar(rootView, "Codewati launches Successfully")
+        Toaster.alertSnackbar(rootView, "Codewati launches Successfully")
+        Toaster.warningSnackbar(rootView, "Codewati launches Successfully")
+        Toaster.infoSnackbar(rootView, "Codewati launches Successfully")
+
+
 
     }
 }
--- customToast/src/main/java/com/codewati/customtoast/Toaster.java
+++ customToast/src/main/java/com/codewati/customtoast/Toaster.java
@@ -1,11 +1,16 @@
 package com.codewati.customtoast;
 
+import static com.google.android.material.snackbar.Snackbar.*;
+
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class Toaster {
 
@@ -27,12 +32,152 @@ public class Toaster {
         text.setText(msg);
 
         ImageView icon = layout.findViewById(R.id.toast_icon);
-        icon.setImageResource(R.drawable.ic_done);
+        icon.setImageResource(R.drawable.svg_done);
 
         Toast toast = new Toast(context);
-        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setDuration(Toast.LENGTH_LONG);
         toast.setView(layout);
         toast.show();
+    }
+
+    // Alert toast
+    public static void alertToast(Context context, String msg) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.custom_alert_toast, null);
+
+        TextView text = layout.findViewById(R.id.alert_message);
+        text.setText(msg);
+
+        ImageView icon = layout.findViewById(R.id.alert_icon);
+        icon.setImageResource(R.drawable.svg_cross);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    // Warning toast
+    public static void warningToast(Context context, String msg) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.custom_warning_toast, null);
+
+        TextView text = layout.findViewById(R.id.warning_message);
+        text.setText(msg);
+
+        ImageView icon = layout.findViewById(R.id.warning_icon);
+        icon.setImageResource(R.drawable.svg_warning);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    // Info toast
+    public static void infoToast(Context context, String msg) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.custom_info_toast, null);
+
+        TextView text = layout.findViewById(R.id.info_message);
+        text.setText(msg);
+
+        ImageView icon = layout.findViewById(R.id.info_icon);
+        icon.setImageResource(R.drawable.svg_info);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    // Loading toast
+    public static void loadingToast(Context context, String msg) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.custom_loading_toast, null);
+
+        TextView text = layout.findViewById(R.id.load_message);
+        text.setText(msg);
+
+        ImageView icon = layout.findViewById(R.id.load_icon);
+        icon.setImageResource(R.drawable.svg_loading);
+
+        Toast toast = new Toast(context);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    // Done Snackbar
+    public static void doneSnackbar(View rootView, String msg) {
+        Snackbar snackbar = Snackbar.make(rootView, "", Snackbar.LENGTH_LONG);
+        View customView = LayoutInflater.from(rootView.getContext()).inflate(R.layout.done_snackbar, null);
+
+        SnackbarLayout snackBarView = (SnackbarLayout) snackbar.getView();
+        snackBarView.setPadding(0, 0, 0, 0);
+        snackBarView.setBackgroundColor(Color.TRANSPARENT);
+
+        // Set message dynamically
+        TextView messageView = customView.findViewById(R.id.success_msg);
+        messageView.setText(msg);
+
+        snackBarView.addView(customView, 0);
+        snackbar.show();
+    }
+
+    // Alert Snackbar
+    public static void alertSnackbar(View rootView, String msg) {
+        Snackbar snackbar = Snackbar.make(rootView, "", Snackbar.LENGTH_LONG);
+        View customView = LayoutInflater.from(rootView.getContext()).inflate(R.layout.alert_snackbar, null);
+
+        SnackbarLayout snackBarView = (SnackbarLayout) snackbar.getView();
+        snackBarView.setPadding(0, 0, 0, 0);
+        snackBarView.setBackgroundColor(Color.TRANSPARENT);
+
+        // Set message dynamically
+        TextView messageView = customView.findViewById(R.id.alert_msg);
+        messageView.setText(msg);
+
+        snackBarView.addView(customView, 0);
+        snackbar.show();
+    }
+
+    // Warning Snackbar
+    public static void warningSnackbar(View rootView, String msg) {
+        Snackbar snackbar = Snackbar.make(rootView, "", Snackbar.LENGTH_LONG);
+        View customView = LayoutInflater.from(rootView.getContext()).inflate(R.layout.warning_snackbar, null);
+
+        SnackbarLayout snackBarView = (SnackbarLayout) snackbar.getView();
+        snackBarView.setPadding(0, 0, 0, 0);
+        snackBarView.setBackgroundColor(Color.TRANSPARENT);
+
+        // Set message dynamically
+        TextView messageView = customView.findViewById(R.id.warning_msg);
+        messageView.setText(msg);
+
+        snackBarView.addView(customView, 0);
+        snackbar.show();
+    }
+
+    // Info Snackbar
+    public static void infoSnackbar(View rootView, String msg) {
+        Snackbar snackbar = Snackbar.make(rootView, "", Snackbar.LENGTH_LONG);
+        View customView = LayoutInflater.from(rootView.getContext()).inflate(R.layout.info_snackbar, null);
+
+        SnackbarLayout snackBarView = (SnackbarLayout) snackbar.getView();
+        snackBarView.setPadding(0, 0, 0, 0);
+        snackBarView.setBackgroundColor(Color.TRANSPARENT);
+
+        // Set message dynamically
+        TextView messageView = customView.findViewById(R.id.info_msg);
+        messageView.setText(msg);
+
+        snackBarView.addView(customView, 0);
+        snackbar.show();
     }
 
 }
